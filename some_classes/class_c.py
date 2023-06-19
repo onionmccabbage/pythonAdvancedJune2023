f8a1b2e4f032af26be61dcc0c5914d7a99fe5662
onionmccabbage <onionmccabbage@live.com>
2023-06-19 12:28:40 +0100

    lunch day 1

--- some_classes/class_c.py
+++ some_classes/class_c.py
@@ -1,13 +1,18 @@
 
+# there is an abstract-base-class (abc) library in Python
+from abc import ABCMeta
 
-class Abstract:
+# but this is not rigorously enforced
+class Abstract(metaclass=ABCMeta):
     '''This is the most generic class possible'''
-
+    
 class Basic(Abstract):
     def __init__(self, x, y):
         self.x = x # we are not obliged to use get/set methods
         self.y = y # careful -these proiperties are directly mutable outside the class
+
 class Concrete(Basic):
+    '''this is a concrete implementation of abstract classes'''
     count = 0 # here is a class property
     def __init__(self, x, y, z):
         super().__init__(x, y)
@@ -22,3 +27,16 @@ if __name__ == '__main__':
     s2 = Concrete('a','b','c')
     s3 = Concrete([], (), {})
     print( Concrete.howMany() ) # we can call class methods on the class or on an instance
+
+    # we can explore intrinsics...
+    print( f'Class name is {Concrete.__name__}' )
+    print( f'Class docstring is {Concrete.__doc__}' )
+    print( f'Class dictionary is {Concrete.__dict__}' )
+    print( f'Class bases are {Concrete.__bases__} and {Abstract.__bases__}' )
+
+
+
+
+
+
+
---
+++ some_classes/user_input.py
@@ -0,0 +1,6 @@
+# remember every user input is a STRING
+
+s = input('how many sides? ')
+c = input('what colour? ')
+
+print(type(s), type(c))
