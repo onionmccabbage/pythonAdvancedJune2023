a92eb65f67f5771f9b04247a85ce69fdfd08093e
onionmccabbage <onionmccabbage@live.com>
2023-06-20 15:38:10 +0100

    sample connection strings

--- database_access/create_db.py
+++ database_access/create_db.py
@@ -24,3 +24,34 @@ def accessDB():
 
 if __name__ == '__main__':
     accessDB()
+
+
+# some other DB conn mechanisms
+# see https://wiki.python.org/moin/DatabaseInterfaces
+# DB2
+# import DB2 # remember to pip install first!!
+# conn = DB2.connect(dsn='ibm-DB', uid='analyst', pwd='db2pwd')
+
+# Sybase
+# import Sybase
+# conn = Sybase.connect('SYBASE', 'username', 'passwd', 'databasename')
+
+# Oracle
+# import cx_Oracle
+# conn = cx_Oracle.connect('username', 'passwd', 'hostname:port/SID')
+# conn2 = cx_Oracle.connect('username/passwd@hostname:portno/SID')
+# dsn_tns = cx_Oracle.makedsn('hostname', portno, 'SID')
+# conn3 = cx_Oracle.connect('username', 'passwd', dsn_tns)
+
+# MySQL
+# import MySQLdb
+# conn = MySQLdb.connect(host = "hostname", user = "username",
+# passwd = "password", db = "dbname")
+
+# PySQLite
+# from pysqlite2 import dbapi2 as sqlite
+# conn = sqlite.connect("mydb", connectionproperties)
+
+# ODBC
+# import odbc
+# conn = odbc. odbc("myDSN/username/password")
