100495c5d4ff9b1c52e17152d3ff97b160d4dbf7
onionmccabbage <onionmccabbage@live.com>
2023-06-22 11:42:15 +0100

    locking threads

--- using_threads/04_thread_locks.py
+++ using_threads/04_thread_locks.py
@@ -11,7 +11,7 @@ def workerA():
     '''this worker will access the global counter to increment it'''
     global counter # we now have access to the global asset called counter
     try:
-        while counter <100:
+        while counter <10:
             counter += 1
             print(f'Worker A is increment counter to {counter}')
             time.sleep(random.randint(0,1)) # a random sleep of 0 or 1
@@ -22,15 +22,21 @@ def workerB():
     '''this worker will access the global counter to decrement it'''
     global counter # we now have access to the global asset called counter
     try:
-        while counter >-100:
+        while counter >-10:
             counter -= 1
             print(f'Worker B is decrement counter to {counter}')
             time.sleep(random.randint(0,1)) # a random sleep of 0 or 1
     except Exception as err:
         print(f'Problem {err}')
 
-
 if __name__ == '__main__':
     # initially try working sequentially
-    workerA()
-    workerB()
+    # workerA()
+    # workerB()
+    tA = threading.Thread(target=workerA)
+    tB = threading.Thread(target=workerB)
+    # both threads try to access the SAME asset - this is dangerous and unpredicable
+    tA.start()
+    tB.start()
+    tA.join()
+    tB.join()
